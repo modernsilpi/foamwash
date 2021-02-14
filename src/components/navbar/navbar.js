c2fbb7a54fcfe25ba9a1894f283ecb7efd23c69f
modernsilpi <modernsilpi@gmail.com>
2021-02-15 00:19:26 +0530

    nav1

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import {Navbar} from 'react-bootstrap';
+import {Navbar, Nav} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Navi = () => {
     return (
         <>
-    <Navbar bg="dark" variant="dark" fixed="top">
+    <Navbar bg="dark" variant="dark" fixed="top" expand="lg">
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
     <Navbar.Brand>
-     <h2>Foam wash</h2>
+     <h3>Foam wash</h3>
     </Navbar.Brand>
+    <Nav className="mr-auto" style={{marginLeft: "50%"}}>
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Services</Nav.Link>
+      <Nav.Link href="#About">About</Nav.Link>
+      <Nav.Link href="#Contact">Contact Us</Nav.Link>
+    </Nav>
+    </Navbar.Collapse>
   </Navbar>
         </>
     )
