0418d4384ca5cca8c7b6cf8f9d0c05d11f967754
modernsilpi <modernsilpi@gmail.com>
2021-02-14 23:56:14 +0530

    navbar

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Navbar} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Navi = () => {
+    return (
+        <>
+    <Navbar bg="dark" variant="dark" fixed="top">
+    <Navbar.Brand>
+     <h2>Foam wash</h2>
+    </Navbar.Brand>
+  </Navbar>
+        </>
+    )
+}
+
+export default Navi
